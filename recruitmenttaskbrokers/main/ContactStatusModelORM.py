0b3d9515f2537a0af9e6d1eec89df6852a585339
Patryk Marchewka <44062662+PatrykMarchewka@users.noreply.github.com>
2026-07-20 22:08:02 +0200

    Add missing documentation
    - Added documentation  to method
    - Added quick return if provided wrong data
    - Ensured that provided value is undercase string in order to avoid accidental duplication of data

--- recruitmenttaskbrokers/main/ContactStatusModelORM.py
+++ recruitmenttaskbrokers/main/ContactStatusModelORM.py
@@ -4,8 +4,18 @@ from .models import ContactStatus
 from .StringManipulator import stripPolishCharacters
 
 
-def getOrCreateStatusByName(status:str) -> ContactStatus:
+def getOrCreateStatusByName(status:str) -> ContactStatus | None:
+    """
+    Returns status for a contact by its name if it exists, otherwise tries to create a new one and save in database
+    :param status: Name of the status to search for
+    :return: ContactStatus if was found or saved in database, None if status is None or couldnt be saved in database
+    """
+    if status is None:
+        return None
+
     status = stripPolishCharacters(status)
+    status = status.lower()
+
     statusFromDB = ContactStatus.objects.filter(name=status).first()
     if statusFromDB is None:
         statusFromDB = ContactStatus(name=status)
