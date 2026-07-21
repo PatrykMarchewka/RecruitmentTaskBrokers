d0823aac5b80a75e7a2bcc197fb4cfb4fa7afa76
Patryk Marchewka <44062662+PatrykMarchewka@users.noreply.github.com>
2026-07-21 23:09:38 +0200

    Update phone regex
    - Updated phone regex to instead use a simplified version
    Old regex approved even partially correct string like '+48123123123abc', new one matches the E164 standard with plus sign, country code and finally the digits.
    Regex must match fully to pass, so +4812312313abc won't pass

--- recruitmenttaskbrokers/main/RegexHelpers.py
+++ recruitmenttaskbrokers/main/RegexHelpers.py
@@ -1,5 +1,5 @@
 from django.core.validators import RegexValidator
 
-#Regex taken from https://regex101.com/r/wZ4uU6/1
-#Should work with any international phone format
-phoneRegexValidator = RegexValidator(regex=r'\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}', message='Invalid phone number format.')
+#Simplified regex, matching the E.164 standard
+#Taken from https://stackoverflow.com/questions/6478875/regular-expression-matching-e-164-formatted-phone-numbers
+phoneRegexValidator = RegexValidator(regex=r'^\+[1-9]\d{1,14}$', message='Invalid phone number format.')
