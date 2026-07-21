44a16db67d2b3d92d84f2a97620bfb42378bc263
Patryk Marchewka <44062662+PatrykMarchewka@users.noreply.github.com>
2026-07-21 23:11:16 +0200

    Updated OpenStreetMap API
    - Updated call to use params instead of hardcoded values in a string
    - Calling OpenStreetMap API for new city now also gets weather for it

--- recruitmenttaskbrokers/main/OpenStreetMapAPI.py
+++ recruitmenttaskbrokers/main/OpenStreetMapAPI.py
@@ -1,8 +1,10 @@
 import json
+from urllib.parse import urlencode
 from urllib.request import urlopen, Request
 
 from django.core.exceptions import ValidationError
 
+from . import OpenMeteoAPI
 from .StringManipulator import stripPolishCharacters
 from .models import City
 
@@ -15,7 +17,13 @@ def _callOpenStreetMapAPI(cityName: str) -> dict | None:
     :param cityName: Name of the city to find
     :return: Dictionary with all fields from JSON response or None if result is empty
     """
-    url = f"https://nominatim.openstreetmap.org/search?q={cityName}&format=json&limit=1"
+    params = urlencode({
+        "q": cityName,
+        "format": "json",
+        "limit": 1,
+    })
+    url = f"https://nominatim.openstreetmap.org/search?{params}"
+
     req = Request(url=url, headers=useragent)
     with urlopen(req) as response:
         result =  json.load(response)[0]
@@ -45,7 +53,7 @@ def _parseOpenStreetMapData(json: dict) -> tuple[str, float, float] | None:
 
 def callOpenStreetMapAPIAndSave(cityName: str) -> City | None:
     """
-    Calls OpenStreetMap API and in case of correct return saves it to database if it doesn't exist there
+    Calls OpenStreetMap API and in case of correct return saves it to database if it doesn't exist there. If city didn't exist in database then additional call to OpenMeteo API is done to get current weather
     :param cityName: Name of the city to find
     :return: City if it was found and saved, otherwise None
     """
@@ -59,7 +67,8 @@ def callOpenStreetMapAPIAndSave(cityName: str) -> City | None:
     #Checking if City already exists in database because saved name is from OpenStreetMapAPI and OpenStreetMap returns city incase of partial match of form
     city = City.objects.filter(name=cityName, lat=latitude, lon=longitude).first()
     if city is None:
-        city = City(name=cityName, lat=latitude, lon=longitude)
+        weather = OpenMeteoAPI.callOpenMeteoAPIAndSave(lat=latitude, lon=longitude)
+        city = City(name=cityName, lat=latitude, lon=longitude, weather=weather)
         try:
             city.full_clean()
             city.save()
