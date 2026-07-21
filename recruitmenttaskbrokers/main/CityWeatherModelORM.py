b8bba013b7552441934b5d1930e8a8acdba41a96
Patryk Marchewka <44062662+PatrykMarchewka@users.noreply.github.com>
2026-07-21 23:17:49 +0200

    Fix timezone issue
    - Fixed a bug as previous timezone.now was offset naive while the timezone from DB was offset aware
    Now both versions are offset aware

--- recruitmenttaskbrokers/main/CityWeatherModelORM.py
+++ recruitmenttaskbrokers/main/CityWeatherModelORM.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta
 
-from .OpenMeteoAPI import callOpenMeteoAPIAndSave
+from django.utils import timezone
 
 from .OpenMeteoAPI import callOpenMeteoAPIAndSave, callOpenMeteoAPIByCityAndSave
 from recruitmenttaskbrokers.main.models import City, CityWeather
@@ -11,7 +11,7 @@ def getWeatherForCity(city:City) -> CityWeather | None:
 
     if recentWeather is None:
         return callOpenMeteoAPIByCityAndSave(city)
-    if recentWeather.createdAt < datetime.now() - timedelta(hours=1):
+    if recentWeather.createdAt < (timezone.now() - timedelta(hours=1)):
         #Most recent weather for this city check was over 1 hour ago, call the API again
         return callOpenMeteoAPIByCityAndSave(city)
     return recentWeather
