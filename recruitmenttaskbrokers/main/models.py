1bf572257051f150a63af7697b502b0da33ad380
Patryk Marchewka <44062662+PatrykMarchewka@users.noreply.github.com>
2026-07-21 23:07:07 +0200

    Changed database models
    - Changed CityWeather to no longer hold connection to City and instead now City holds connection to CityWeather
    Old: Contact -> City <- CityWeather
    New: Contact -> City -> CityWeather

--- recruitmenttaskbrokers/main/models.py
+++ recruitmenttaskbrokers/main/models.py
@@ -2,26 +2,26 @@ from django.db import models
 
 from .RegexHelpers import phoneRegexValidator
 
-
-class City(models.Model):
-    ID = models.BigAutoField(primary_key=True)
-    name = models.CharField(max_length=200, unique=True)
-    lat = models.FloatField()
-    lon = models.FloatField()
-
-    def __str__(self):
-        return f'{self.name} - {self.lat}lat {self.lon}lon'
-
 class CityWeather(models.Model):
     ID = models.BigAutoField(primary_key=True)
-    city = models.ForeignKey(City, on_delete=models.CASCADE)
     temperature = models.FloatField()
     humidity = models.FloatField()
     windSpeed = models.FloatField()
     createdAt = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'{self.city.name}: Temperature {self.temperature}C, Humidity {self.humidity}%, Wind speed {self.windSpeed}kmh'
+        return f'Temperature {self.temperature}C, Humidity {self.humidity}%, Wind speed {self.windSpeed}kmh'
+
+class City(models.Model):
+    ID = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=200, unique=True)
+    lat = models.FloatField()
+    lon = models.FloatField()
+    weather = models.ForeignKey(CityWeather, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return f'{self.name} - {self.lat}lat {self.lon}lon'
 
 class ContactStatus(models.Model):
     ID = models.BigAutoField(primary_key=True)
