c215830027cb1308827706dbe8876b6b66d0bd9e
Patryk Marchewka <44062662+PatrykMarchewka@users.noreply.github.com>
2026-07-23 20:16:51 +0200

    Remove admin panel page
    - Removeed admin panel page that was added by default

--- recruitmenttaskbrokers/urls.py
+++ recruitmenttaskbrokers/urls.py
@@ -14,14 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
 
 from recruitmenttaskbrokers.main.views import *
 from recruitmenttaskbrokers.main.RestAPI.views import *
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('contacts/contactList/', ContactListGet, name='contactList'),
     path('contacts/contactEdit/', ContactCreate, name='contactCreate'),
     path('contacts/contactEdit/<int:contactID>', ContactEdit, name='contactEdit'),
