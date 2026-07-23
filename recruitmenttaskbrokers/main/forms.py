ecec1c85e1f8de939765906643022c474866da92
Patryk Marchewka <44062662+PatrykMarchewka@users.noreply.github.com>
2026-07-23 22:53:08 +0200

    Add widgets for frontend validation
    - Added frontend validation when adding/editing contat

--- recruitmenttaskbrokers/main/forms.py
+++ recruitmenttaskbrokers/main/forms.py
@@ -1,15 +1,34 @@
 from django import forms
 from django.core.validators import EmailValidator
 
-from .RegexHelpers import phoneRegexValidator
+from .RegexHelpers import phoneRegexValidator, PHONE_REGEX_STRING
 from .models import Contact
 
 class ContactForm(forms.ModelForm):
-    city = forms.CharField()
-    status = forms.CharField()
+    city = forms.CharField(
+        widget=forms.TextInput(attrs={"class": "form-control"})
+    )
+
+    status = forms.CharField(
+        widget=forms.TextInput(attrs={"class": "form-control"})
+    )
+
     class Meta:
         model = Contact
         fields = ['name', 'lastName', 'email', 'phone'] #city and status omitted
+        #Widget to validate phone in frontend using same regex as the one used in backend, rest added to make sure all fields look the same
+        widgets = {
+            "name": forms.TextInput(attrs={"class": "form-control"}),
+            "lastName": forms.TextInput(attrs={"class": "form-control"}),
+            "email": forms.EmailInput(attrs={"class": "form-control"}),
+            "phone": forms.TextInput(attrs={
+                "type": "text",
+                "pattern": PHONE_REGEX_STRING,
+                "title": "Phone number must start with + and contain only digits",
+                "class": "form-control"}),
+            "city": forms.TextInput(attrs={"class": "form-control"}),
+            "status": forms.TextInput(attrs={"class": "form-control"})
+        }
 
     def __init__(self, *args, **kwargs):
         contact = kwargs.get('instance')
