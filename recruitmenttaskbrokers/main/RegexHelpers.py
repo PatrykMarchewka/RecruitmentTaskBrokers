1572cdb859d9b63c6a1b4fded7599940937fd765
Patryk Marchewka <44062662+PatrykMarchewka@users.noreply.github.com>
2026-07-23 22:51:49 +0200

    Move regex into its own const string
    - Moved regex into its own string to ensure it can be used as string and not full validator in other places

--- recruitmenttaskbrokers/main/RegexHelpers.py
+++ recruitmenttaskbrokers/main/RegexHelpers.py
@@ -2,4 +2,5 @@ from django.core.validators import RegexValidator
 
 #Simplified regex, matching the E.164 standard
 #Taken from https://stackoverflow.com/questions/6478875/regular-expression-matching-e-164-formatted-phone-numbers
-phoneRegexValidator = RegexValidator(regex=r'^\+[1-9]\d{1,14}$', message='Invalid phone number format.')
+PHONE_REGEX_STRING = r'^\+[1-9]\d{1,14}$'
+phoneRegexValidator = RegexValidator(regex=PHONE_REGEX_STRING, message='Invalid phone number format.')
